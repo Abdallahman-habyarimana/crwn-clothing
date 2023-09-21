33b18b6275206f542f62492612bcd2c1ad3538f5
Abdallahman Habyarimana <habdallah005@gmail.com>
2023-09-21 18:00:03 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'sneakers'
+    },
+    {
+      id:  3,
+      title: 'Jackets'
+    },
+    {
+      id: 4,
+      title: 'Mens'
+    },
+    {
+      id: 5,
+      title: 'Womens'
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+        { categories.map((category )=> ( 
+            <div className='.category-container ' key={category.id}>
+              <div className='category-body-container'>
+                <h2>{category.title}</h2>
+                <p>Shop Now</p>
+              </div>
+            </div>
+        ))}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
