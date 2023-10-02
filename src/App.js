7573744d9da74b6b16bcd50eedd0aa0e15b39dcd
Abdallahman Habyarimana <habdallah005@gmail.com>
2023-10-02 18:17:17 -0400

    Implementing react router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Directory from './components/directory/directory.component'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
+
+const Shop = () => {
+  return (
+    <h1>I am the shopping page</h1>
+  )
+}
 
 const App = () => {  
   return (
-    <>
-      <Directory />
-    </>
+    <Routes>
+      <Route path="/" element={ <Navigation />}>
+        <Route index element={ <Home /> } />
+        <Route path='shop' element={ <Shop />} />
+      </Route>
+     
+    </Routes>
   )
 }
 
